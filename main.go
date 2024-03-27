da8b769afe484262c81201a6213fdcde4f22d30d
jparouca <joaopearoucaa@gmail.com>
2024-03-27 04:04:13 -0300

    fuck htttp me and my bois are websocket, add ping to active connections

--- main.go
+++ main.go
@@ -10,11 +10,17 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// fuck http connections me and my bois are web sockets
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
+type ClientData struct {
+	Nickname string
+}
+
+// active connections and nick
 var clients sync.Map
 
 func main() {
@@ -25,8 +31,16 @@ func main() {
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// prevent connection refused, everyone is welcome, even chinese bots
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
+	nickname := r.URL.Query().Get("nickname")
+	if nickname == "" {
+		nickname = "Anonymous"
+	}
+
+
+// fuck http connections me and my bois are web sockets
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -34,32 +48,69 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	clients.Store(conn, true)
+	clientData := &ClientData{Nickname: nickname}
+	clients.Store(conn, clientData)
 
+	//read message
 	for {
-		_, _, err := conn.ReadMessage()
+		messageType, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
 			break
 		}
+
+		if messageType == websocket.TextMessage {
+			var data map[string]interface{}
+			err = json.Unmarshal(message, &data)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			if action, ok := data["action"].(string); ok {
+				if action == "ping" {
+					targetNickname, ok := data["targetNickname"].(string)
+					if !ok {
+						log.Println("Invalid 'targetNickname' field in the message")
+						continue
+					}
+
+					clients.Range(func(key, value interface{}) bool {
+						clientData := value.(*ClientData)
+						if clientData.Nickname == targetNickname {
+							pingMessage := []byte("ping")
+							conn := key.(*websocket.Conn)
+							conn.WriteMessage(websocket.TextMessage, pingMessage)
+						}
+						return true
+					})
+				}
+			}
+		}
 	}
 
+	// Remove the client from the clients map when the connection is closed
 	clients.Delete(conn)
 }
 
+// updateActiveConnections periodically sends the list of active connections to all connected clients
 func updateActiveConnections() {
 	for {
-		activeConnections := make([]string, 0)
-
+		// connected bois
+		var activeConnections []string
 		clients.Range(func(key, value interface{}) bool {
-			// key is the connection, value is not used
-			activeConnections = append(activeConnections, "Active Connection")
+			clientData := value.(*ClientData)
+			activeConnections = append(activeConnections, clientData.Nickname)
 			return true
 		})
 
-		message, err := json.Marshal(map[string]interface{}{
+		// map with the active connections and nick
+		data := map[string]interface{}{
 			"activeConnections": activeConnections,
-		})
+		}
+
+		// marshal jasuon boy
+		message, err := json.Marshal(data)
 		if err != nil {
 			log.Println(err)
 			return
@@ -75,6 +126,6 @@ func updateActiveConnections() {
 			return true
 		})
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
